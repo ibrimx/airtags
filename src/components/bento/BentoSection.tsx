e2d459f2434a2d279358fbeba4da511f143fb640
Ibrim <159613319+ibrimx@users.noreply.github.com>
2026-02-22 14:36:29 +0300

    Update BentoSection.tsx

--- src/components/bento/BentoSection.tsx
+++ src/components/bento/BentoSection.tsx
@@ -1,24 +1,10 @@
-import BentoLayout from "./BentoLayout"
-import BentoCard from "./BentoCard"
+// components/bento/BentoSection.tsx
+
 import { getBentoBlocks } from "@/lib/notion/bento"
+import BentoLayout from "./BentoLayout"
 
 export default async function BentoSection() {
   const blocks = await getBentoBlocks()
 
-  return (
-    <AutoLayout spans={blocks.map(b => b.span)}>
-      {blocks.map(block => (
-        <Card
-          key={block.id}
-          layout={block.layout}
-          title={block.title}
-          description={block.description}
-          image={block.image}
-          linkText={block.linkText}
-          linkHref={block.linkHref}
-          styleVariant={block.styleVariant}
-        />
-      ))}
-    </AutoLayout>
-  )
+  return <BentoLayout blocks={blocks} />
 }
